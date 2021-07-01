d0d97786dbcf2b2e2b65580128dffe2e3011b868
spek13 <171221@ids.upchiapas.edu.mx>
2021-07-01 16:19:27 -0500

    first commit

---
+++ src/main/java/mx/com/citibanamex/CRUD/CrudApplication.java
@@ -0,0 +1,13 @@
+package mx.com.citibanamex.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
